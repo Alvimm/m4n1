36c744de6952c80aa983fba926bc6cdd2b6fc678
alvimm <filipe2012alvim@gmail.com>
2024-02-25 13:44:08 -0300

    Add BottomTabs navigation component

---
+++ Navigations/BottomTabs.jsx
@@ -0,0 +1,27 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Cadastro from '../Screens/Cadastro';
+import CadastrosProd from '../Screens/CadastrosProd';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const Tab = createBottomTabNavigator();
+
+export default function BottomTabs() {
+  return (
+    <Tab.Navigator screenOptions={{
+      headerShown: false,
+      tabBarStyle: { height: 60, paddingBottom: 5, paddingTop: 5},
+      tabBarLabelStyle: { fontSize: 14}
+    }}>
+      <Tab.Screen name="home" component={Cadastro} options={{
+        tabBarIcon: ({ color, size }) => (
+          <Icon name="home" color={color} size={size} />
+        ),
+      }} />
+      <Tab.Screen name="produtos" component={CadastrosProd} options={{
+        tabBarIcon: ({ color, size }) => (
+          <Icon name="list" color={color} size={size} />
+        ),
+      }} />
+    </Tab.Navigator>
+  );
+}
