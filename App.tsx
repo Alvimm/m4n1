886f0a52ae59d1e4f27ddb70ec783903e417f52f
alvimm <filipe2012alvim@gmail.com>
2024-03-05 23:29:30 -0300

    Hide status bar in App component

--- App.tsx
+++ App.tsx
@@ -9,8 +9,7 @@ function App(): React.JSX.Element {
   return (
     <View style={styles.container}>
       <StatusBar
-        animated={true}
-        backgroundColor="#2ADBE5"
+        hidden={true}
       />
       <NavigationContainer>
         <BottomTabs />
