19f876dda5502d475b82922e6e793fa7dc1a7e89
alvimm <filipe2012alvim@gmail.com>
2024-03-07 15:49:54 -0300

    Refactor Cadastro and ListaFornecedores screens

---
+++ src/screens/Cadastro.jsx
@@ -0,0 +1,165 @@
+import { useNavigation } from '@react-navigation/native';
+import { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, Image, ToastAndroid, View, ScrollView, Alert } from 'react-native';
+import { launchImageLibrary, launchCamera } from 'react-native-image-picker';
+import Icon from 'react-native-vector-icons/Ionicons';
+import  CustomTextInput  from '../components/CustomTextInput';
+
+
+const Cadastro = () => {
+  const [nome, setNome] = useState('');
+  const [endereco, setEndereco] = useState('');
+  const [contato, setContato] = useState('');
+  const [categoria, setCategoria] = useState('');
+  const [dadosCadastro, setDadosCadastro] = useState([]);
+  const [imagem, setImagem] = useState('');
+  const navigation = useNavigation();
+
+  const showToast = () => {
+    ToastAndroid.showWithGravityAndOffset(
+      'Preencha todos os campos',
+      ToastAndroid.SHORT,
+      ToastAndroid.BOTTOM,
+      25,
+      50
+    );
+  }
+  const ImagePicker = () => {
+    let options = {
+      storageOptions: {
+        path: 'image',
+      },
+    };
+
+    Alert.alert(
+      'Escolha uma opção',
+      'Deseja abrir a câmera ou a biblioteca de imagens?',
+      [
+        {
+          text: 'Cancelar',
+          style: 'cancel',
+        },
+        {
+          text: 'Abrir câmera',
+          onPress: () => launchCamera(options, (response) => {
+            if (response.assets && response.assets[0].uri) {
+              setImagem(response.assets[0].uri);
+            }
+          }),
+        },
+        {
+          text: 'Abrir biblioteca de imagens',
+          onPress: () => launchImageLibrary(options, (response) => {
+            if (response.assets && response.assets[0].uri) {
+              setImagem(response.assets[0].uri);
+            }
+          }),
+        },
+      ],
+      { cancelable: true },
+    );
+  };
+
+  const handleCadastrar = () => {
+    if (nome !== '' && endereco !== '' && contato !== '' && categoria !== '' && imagem !== '') {
+      const novoCadastro = { nome, endereco, contato, categoria, imagem };
+      const novosDadosCadastro = [...dadosCadastro, novoCadastro];
+      setDadosCadastro(novosDadosCadastro);
+      navigation.navigate('fornecedores', { dadosCadastro: novosDadosCadastro });
+      setNome('');
+      setEndereco('');
+      setContato('');
+      setCategoria('');
+      setImagem('');
+    } else {
+      showToast();
+    }
+  };
+  return (
+    <ScrollView style={styles.container}>
+      <Image source={require('../assets/logo.png')} style={styles.logo} />
+      <CustomTextInput placeholder="Digite o nome" onChangeText={setNome} value={nome}/>
+      <CustomTextInput placeholder="Digite o endereço" onChangeText={setEndereco} value={endereco} />
+      <CustomTextInput placeholder="Digite o contato" onChangeText={setContato} value={contato} inputMode='tel' maxLength={13} />
+      <CustomTextInput placeholder="Digite a categoria" onChangeText={setCategoria} value={categoria} />
+
+      <TouchableOpacity style={styles.btnImg} onPress={ImagePicker} >
+        <Text style={styles.btnText}>Escolha sua foto</Text>
+        {imagem && (
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <Image
+              source={{ uri: imagem }}
+              style={styles.selectedImg}
+            />
+            <Icon name="checkmark-circle" color={'green'} size={20} />
+          </View>
+        )}
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.btn}
+        onPress={() => {
+          handleCadastrar();
+        }}
+      ><Text style={styles.btnText}>Cadastrar</Text></TouchableOpacity>
+    </ScrollView>
+  );
+};
+
+export default Cadastro;
+
+const styles = StyleSheet.create(
+  {
+    container: {
+      paddingHorizontal: 20,
+      backgroundColor: '#fa4b77',
+      paddingVertical: 50,
+    },
+    text: {
+      fontFamily: 'Anta-Regular',
+      fontSize: 20,
+      color: '#000',
+    },
+    logo: {
+      width: 150,
+      height: 150,
+      alignSelf: 'center',
+      marginTop: 50,
+      marginBottom: 10
+    },
+    btnImg: {
+      alignItems: 'center',
+      marginTop: 10,
+      backgroundColor: '#f64c81',
+      borderColor: '#000',
+      borderWidth: 1,
+      padding: 10,
+      borderRadius: 5,
+      flexDirection: 'row',
+      justifyContent: 'center',
+    },
+    btn: {
+      alignItems: 'center',
+      marginTop: 30,
+      backgroundColor: '#FF5474',
+      padding: 20,
+      borderRadius: 5,
+      borderColor: '#000',
+      borderWidth: 1,
+      marginBottom: 60,
+    },
+    btnText: {
+      fontFamily: 'Anta-Regular',
+      color: '#000',
+      fontSize: 22,
+    },
+    selectedImg:{
+      width: 30,
+      height: 30,
+      borderColor: 'green',
+      borderWidth: 2,
+      borderRadius: 5,
+      marginLeft: 10,
+      marginRight: 2
+    }
+  });
---
+++ src/screens/ListaFornecedores.jsx
@@ -0,0 +1,89 @@
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useMemo } from 'react';
+import SearchBar from '../components/SearchBar';
+
+const ListaFornecedores = ({ route }) => {
+  const [dadosCadastro, setDadosCadastro] = useState(route.params ? route.params.dadosCadastro : []);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    setDadosCadastro(route.params ? route.params.dadosCadastro : []);
+  }, [route]);
+
+  const filteredData = useMemo(() => {
+    if (!search) return dadosCadastro;
+    return dadosCadastro.filter((item) => {
+      return (
+        item.nome.toLowerCase().includes(search.toLowerCase())
+      );
+    });
+  }, [dadosCadastro, search]);
+
+  return (
+    <View style={styles.container}>
+      <SearchBar search={search} setSearch={setSearch} />
+      <FlatList
+        style={styles.listContainer}
+        showsVerticalScrollIndicator={false}
+        data={filteredData}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.card}>
+              <Image source={{ uri: item.imagem }} style={styles.image} />
+              <View style={styles.textContainer}>
+                <Text style={{ fontFamily: 'Anta-Regular', color: 'gray', fontSize: 13 }}>{`${item.contato}`}</Text>
+                <Text style={styles.cardText}>{`${item.nome}`}</Text>
+                <Text style={styles.categoryText}>{`${item.categoria}`}</Text>
+                <Text style={{ fontFamily: 'Anta-Regular', color: 'gray', fontSize: 16 }}>{`${item.endereco}`}</Text>
+              </View>
+            </View>
+          )
+        }}
+      />
+    </View>
+  );
+};
+
+export default ListaFornecedores;
+
+const styles = StyleSheet.create({
+  container: { flex: 1, 
+    backgroundColor: '#fa4b77',
+    paddingTop: 10,
+    paddingHorizontal:10
+   },
+  listContainer: {
+    padding: 10,
+  },
+  card: {
+    flexDirection: 'row',
+    marginVertical: 10,
+    backgroundColor: '#0008',
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  cardText: {
+    fontFamily: 'Anta-Regular',
+    fontSize: 30,
+    color: '#fff',
+  },
+  categoryText: {
+    fontFamily: 'Anta-Regular',
+    color: '#fff',
+    fontSize: 20,
+    backgroundColor: '#fff3',
+    borderRadius: 5,
+    padding: 4,
+    alignSelf: 'flex-start',
+  },
+  textContainer: {
+    marginLeft: 15,
+  },
+  image: {
+    width: 150,
+    height: 150,
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10,
+  }
+});
