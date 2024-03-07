20f4cc7258f5f19346f8945950c4f44c9fdf65b1
alvimm <filipe2012alvim@gmail.com>
2024-03-07 15:49:25 -0300

    Update imports and StatusBar in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet, View, StatusBar } from 'react-native';
-import BottomTabs from './components/BottomTabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { useKeepAwake } from '@sayem314/react-native-keep-awake';
+import BottomTabs from './src/components/BottomTabs';
 
 
 function App(): React.JSX.Element {
@@ -9,7 +9,8 @@ function App(): React.JSX.Element {
   return (
     <View style={styles.container}>
       <StatusBar
-        hidden={true}
+        animated={true}
+        backgroundColor="#fa4b77"
       />
       <NavigationContainer>
         <BottomTabs />
