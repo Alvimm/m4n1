cf8b4a79d8c59fa6eac36193b34c877a70d43e85
alvimm <filipe2012alvim@gmail.com>
2024-03-10 17:12:25 -0300

    Remove unused import statement

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { StyleSheet, TextInput, View, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
-import Filter from './Filter';
 
 const SearchBar = ({ search, setSearch }) => {
   const [isSearchActive, setIsSearchActive] = useState(false);
@@ -29,7 +28,6 @@ const SearchBar = ({ search, setSearch }) => {
           </TouchableOpacity>
         )}
       </View>
-          <Filter />
     </View>
   )
 };
