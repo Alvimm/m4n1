d8d0b7f80027a0f633cf04a699e6bed7695387a9
alvimm <filipe2012alvim@gmail.com>
2024-04-22 14:39:14 -0300

    Update dependencies and remove unused code

--- App.tsx
+++ App.tsx
@@ -1,12 +1,10 @@
 import { StyleSheet, View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { useKeepAwake } from '@sayem314/react-native-keep-awake';
 import BottomTabs from './src/components/BottomTabs';
 import Colors from './src/assets/Colors';
 
 
 function App(): React.JSX.Element {
-  useKeepAwake();
   return (
     <View style={styles.container}>
       <StatusBar
